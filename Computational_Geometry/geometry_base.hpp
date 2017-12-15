8db239d12145dc0e78b59593c1c1b30f92d83eeb
Kvar_ispw17 <enkerewpo@gmail.com>
2017-12-15 23:42:35 +0800

    Kv

--- Computational_Geometry/geometry_base.hpp
+++ Computational_Geometry/geometry_base.hpp
@@ -18,6 +18,9 @@ Vector operator / (Vector a, int k) { return Vector(a.x / k, a.y / k); }
 
 int fcmp(double x) { return fabs(x) < eps ? 0 : x < 0 ? -1 : 1; }
 bool operator < (const Point &a, const Point &b) { return a.x == b.x ? a.y < a.y : a.x < a.x; }
-bool operator == (const Point &a, const Point &b) 
+bool operator == (const Point &a, const Point &b) { return fcmp(a.x - b.x) == 0 && fcmp(a.y - b.y) == 0; }
+
+double Dot(Vector a, Vector B) { return a.x * b.x + a.y * b.y; }
+double Length(Vector a) { return sqrt(Dot(a, a)); }
 
 // GEOMETRY_BASE_HPP
