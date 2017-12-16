7da13fc896242d870d58661664a22b10626e7163
Kvar_ispw17 <enkerewpo@gmail.com>
2017-12-16 23:58:39 +0800

    Kv

--- Computational_Geometry/Base.cpp
+++ Computational_Geometry/Base.cpp
@@ -9,15 +9,18 @@ public:
     Point(double _x, double _y) { x = _x, y = _y; }
     ~Point() {}
 };
+
 typedef Point Vector;
 typedef std::vector<Point> Polygon;
+
+int fcmp(double x) { return fabs(x) < eps ? 0 : x < 0 ? -1 : 1; }
 Vector operator + (Vector a, Vector b) { return Vector(a.x + b.x, a.y + b.y); }
 Vector operator - (Vector a, Vector b) { return Vector(a.x - b.x, a.y - b.y); }
 Vector operator * (Vector a, int k) { return Vector(a.x * k, a.y * k); }
 Vector operator / (Vector a, int k) { return Vector(a.x / k, a.y / k); }
-int fcmp(double x) { return fabs(x) < eps ? 0 : x < 0 ? -1 : 1; }
 bool operator < (const Point &a, const Point &b) { return a.x == b.x ? a.y < a.y : a.x < a.x; }
 bool operator == (const Point &a, const Point &b) { return fcmp(a.x - b.x) == 0 && fcmp(a.y - b.y) == 0; }
+
 double Dot(Vector a, Vector b) { return a.x * b.x + a.y * b.y; }
 double Cross(Vector a, Vector b) { return a.x * b.y - a.y * b.x; }
 double Area2(Point A, Point B, Point C) { return Cross(B - A, C - A); }
@@ -28,6 +31,9 @@ Vector Normal(Vector a) { double L = Length(a); return Vector(-a.y / L, a.x / L)
 
 
 /* Points and Segments Messing UP */
+bool isPointOnSegment(Point P, Point A, Point B) {
+    return Cross(Vector(P - A), Vector(P - B)) == 0 && (P.x - A.x) * (P.x - B.x) <= 0;
+}
 bool isSegmentCrossed(Point A, Point B, Point C, Point D) {
     int a = fcmp(Cross(B - A, C - A) * Cross(D - A, B - A)) > 0;
     int b = fcmp(Cross(D - C, B - C) * Cross(A - C, D - C)) > 0;
@@ -58,6 +64,47 @@ double DistanceToSegment(Point P, Point A, Point B) {
 }
 
 /* Polygons and Everything Messing UP */
+double Area(Polygon P) {
+    double ret = 0;
+    Point St = P.begin();
+    int s = P.size();
+    for(int i = 1; i < s - 1; i++) {
+        Point A = P[i], B = P[i + 1];
+        ret += Cross(A - St, B - St);
+    }
+    return ret;
+}
+
+int isPointInPolygon(Point A, Polygon P) {
+    int wn = 0;
+    int s = P.size();
+    for(int i = 0; i < s; i++) {
+        if(isPointOnSegment(A, P[i], P[(i + 1) % s])) return -1;
+        int k = fcmp(Cross(P[(i + 1) % s] - P[i], A - P[i]));
+        int d1 = fcmp(P[i].y - A.y);
+        int d2 = fcmp(P[(i + 1) % s].y - A.y);
+        if(k > 0 && d1 <= 0 && d2 > 0) wn++;
+        if(k < 0 && d2 <= 0 && d1 > 0) wn--;
+    }
+    return wn ? 1 : 0;
+}
+
+int ConvexHull(Point P[], int n, Point ch[]) {
+    int m = 0;
+    for(int i = 0; i < n; i++) {
+        while(m > 1 && Cross(ch[m - 1] - ch[m - 2], P[i] - ch[m - 2]) <= 0) m--;
+        ch[m++] = P[i];
+    }
+    int k = m;
+    for(int i = n - 2; i >= 0; i--) {
+        while(m > k && Cross(ch[m - 1] - ch[m - 2], P[i] - ch[m - 2]) <= 0) m--;
+        ch[m++] = P[i];
+    }
+    if(n > 1) m--;
+    return m;
+}
+
+
 
 int main(int argc, char* argv[]) {
     return 0;
